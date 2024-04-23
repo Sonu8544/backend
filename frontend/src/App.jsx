73e3a1d641f2f88cda57915ec046dd228a4ee07a
Sonu Kumar <sksonu8544@gmail.com>
2024-04-24 00:40:03 +0530

    setough front end

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { useState } from 'react'
+
+function App() {
+  const [jokes, setJokes] = useState([])
+
+  return (
+    <>
+      <h1>Sonu kumar start backend</h1>
+    </>
+  )
+}
+
+export default App
