b6ec0a1014508223330d2777e421d975f54b4550
Sonu Kumar <sksonu8544@gmail.com>
2025-04-27 21:28:13 +0530

    forgot password api

--- src/app.js
+++ src/app.js
@@ -6,10 +6,6 @@ const cookieParser = require("cookie-parser");
 app.use(express.json());
 app.use(cookieParser());
 
-// const authRouter = require("./routes/auth");
-// const profileRouter = require("./routes/profile");
-// const requestRouter = require("./routes/request");
-
 // const userAuth = require("./middlewares/auth.js");
 const authRouter = require("./routes/auth.js");
 const profileRouter = require("./routes/profile.js");
